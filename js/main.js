613663989114a97cccc5eb13a8f41c8776ef4438
Lesha Shamrytskyi <lesha.shamryzkiy@gmail.com>
2017-11-28 00:14:42 +0200

    add ajax-serch

--- js/main.js
+++ js/main.js
@@ -7,7 +7,7 @@ var hwNeedLinks = true;
 $(document).ready(function(e) {
 	$('.slide').css(
 		{"position" : "absolute",
-		 "top":'0', "left": '0'}).hide().eq(0).show();
+		 "top":'0', "left": '0', "z-index": '9999', }).hide().eq(0).show();
 	var slideNum = 0;
 	var slideTime;
 	slideCount = $("#slider .slide").size();
@@ -65,20 +65,79 @@ var $linkArrow = $('<a id="prewbutton" href="#">&lt;</a><a id="nextbutton" href=
 
 
 //Живой поиск
+
+
+var langs = ["ActionScript", "AppleScript", "Asp", "BASIC", "C", "C++", "Clojure", "COBOL", "ColdFusion", "Erlang", "Fortran", "Groovy", "Haskell", "Java", "JavaScript", "Lisp", "Perl", "PHP", "Python", "Ruby", "Scala", "Scheme"];
+
 $('#search').bind("change keyup input click", function() {
-    if(this.value.length >= 2){
-        $.ajax({
-            type: 'post',
-            url: "search.php", //Путь к обработчику
-            data: {'referal':this.value},
-            response: 'text',
-            success: function(data){
-                $(".search_result").html(data).fadeIn(); //Выводим полученые данные в списке
-           }
-       })
-    }
+
+	if(this.value.length >= 1){
+		var searchval = this.value;
+		var searchvalLower = searchval.toLowerCase();
+		var filter = [];
+		for(var i in langs){
+			var name = langs[i];
+			var nameLover = name.toLowerCase();
+			if(nameLover.indexOf(searchvalLower) > -1){
+				filter.push(name);
+			}
+		}
+
+		// console.log(searchval);
+		// console.log(filter);
+		// console.log(data);
+
+		var list = [];
+		for (var i in filter) {
+			name = filter[i];
+			name = name.split(searchval).join('<b>'+searchval+'</b>');
+			list.push('<li>'+name+'</li>');
+		}
+		$(".search_result").html(list).fadeIn();
+	} else {
+		$(".search_result").fadeOut();
+	}
+
 })
-    
+
+
+// $('#search').bind("change keyup input click", function() {
+// 	var searchval = this.value;
+//     if(this.value.length >= 1){
+//         $.ajax({
+//             type: 'post',
+//             url: "search.html", //Путь к обработчику
+//             data: {'serchinput':this.value},
+//             response: 'text',
+//             success: function(data){
+//             	data = JSON.parse(data);
+            	
+//             	var filter = [];
+//             	// var searchvalLower = searchval.toLowerCase();
+//             	for(var i in data){
+// 					var name = data[i];
+//             		if(name.toLowerCase().indexOf(searchval) > -1){
+// 						filter.push(name);
+// 					}
+//             	}
+//             	// console.log(searchval);
+//             	// console.log(filter);
+//             	// console.log(data);
+
+//             	var list = [];
+//             	for(var i in filter){
+//             		name = filter[i];
+//             		name = name.split(searchval).join('<b>'+searchval+'</b>');
+// 					list.push('<li>'+name+'</li>');
+//             	}
+
+
+//                 $(".search_result").html(list).fadeIn(); //Выводим полученые данные в списке
+//            }
+//        })
+//     }
+// })
+
 $(".search_result").hover(function(){
     $("#search").blur(); //Убираем фокус с input
 })
@@ -86,7 +145,7 @@ $(".search_result").hover(function(){
 //При выборе результата поиска, прячем список и заносим выбранный результат в input
 $(".search_result").on("click", "li", function(){
     s_user = $(this).text();
-    //$(".who").val(s_user).attr('disabled', 'disabled'); //деактивируем input, если нужно
+    $("#search").val(s_user); //деактивируем input, если нужно
     $(".search_result").fadeOut();
 })
 
--- search.php
+++ search.php
@@ -1,24 +1,37 @@
 <?php
 
+
 define("DB_HOST","localhost");
-define("DB_NAME",""); //Имя базы
-define("DB_USER",""); //Пользователь
+define("DB_NAME","world"); //Имя базы
+define("DB_USER","root"); //Пользователь
 define("DB_PASSWORD",""); //Пароль
 define("PREFIX",""); //Префикс если нужно
 
 $mysqli = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
 $mysqli -> query("SET NAMES 'utf8'") or die ("Ошибка соединения с базой!");
 
-if(!empty($_POST["referal"])){ //Принимаем данные
-
-    $referal = trim(strip_tags(stripcslashes(htmlspecialchars($_POST["referal"]))));
-
-    $db_referal = $mysqli -> query("SELECT * from ".PREFIX."search WHERE name LIKE '%$referal%'")
-    or die('Ошибка №'.__LINE__.'<br>Обратитесь к администратору сайта пожалуйста, сообщив номер ошибки.');
-
-    while ($row = $db_referal -> fetch_array()) {
-        echo "\n<li>".$row["name"]."</li>"; //$row["name"] - имя поля таблицы
-    }
-
+if(!empty($_POST["serchinput"])){
+	$serchinput = $_POST["serchinput"];
+}elseif(!empty($_GET["serchinput"])){
+	$serchinput = $_GET["serchinput"];
+}else{
+	return null;
 }
+
+
+
+$referal = trim(strip_tags(stripcslashes(htmlspecialchars($serchinput))));
+
+$db_referal = $mysqli->query("SELECT Name FROM city WHERE Name LIKE '%".$referal."%' LIMIT 10");
+//$db_referal = $mysqli->query("SELECT Name FROM city");
+
+$list = array();
+while ($row = $db_referal -> fetch_array()) {
+   //echo "\n<li>".$row['Name']."</li>"; //$row["name"] - имя поля таблицы
+	$list[] = "\"".$row['Name']."\"";
+}
+
+echo "[".implode(",", $list)."]";
+
+
 ?>
